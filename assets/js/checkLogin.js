9ccc71d02f04795bb7a22cb2191752123fce715f
shubhadip-raj <shubhadipraj2000@gmail.com>
2025-05-19 11:09:13 +0530

    commit

--- assets/js/checkLogin.js
+++ assets/js/checkLogin.js
@@ -163,6 +163,13 @@
 // window.onresize = function () {
 //     toggleMenu();
 // };
+// Function to get a cookie by name
+// function getCookie(name) {
+//     const value = `; ${document.cookie}`;
+//     const parts = value.split(`; ${name}=`);
+//     if (parts.length === 2) return decodeURIComponent(parts.pop().split(';').shift());
+//     return null;
+//   }
 
 
 
